20b57b5253e25543356dbd1c88855c96dc75c4b1
Katharina Herges <katharina-herges@gmx.de>
2026-04-22 15:19:11 +0200

    new histogram script, with further splitting into category ids (etau, mutau, tautau)

---
+++ first_histograms/background_overview_category_id.py
@@ -0,0 +1,108 @@
+import awkward as ak
+import hist
+from hist import Hist
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""This script plots HH signal, tt and dy background data in the HH output node,
+with further splitting the tt background into its six different category ids.
+"""
+events_dy = ak.from_parquet("/data/dust/user/wolfmor/hh2bbtautau/vincent/dy_22pre_v14.parquet")  # dy simulation data
+events_tt = ak.from_parquet("/data/dust/user/wolfmor/hh2bbtautau/vincent/tt_22pre_v14.parquet")  # tt simulation data
+events_hh = ak.from_parquet("/data/dust/user/wolfmor/hh2bbtautau/vincent/hh_22pre_v14.parquet")  # hh simulation data
+
+
+n_bins = 100
+
+# initialize histograms
+hh = Hist(hist.axis.Regular(n_bins, 0, 1, name="hh", label="hh"))
+dy = Hist(hist.axis.Regular(n_bins, 0, 1, name="dy", label="dy"))
+tt = Hist(hist.axis.Regular(n_bins, 0, 1, name="tt", label="tt"))
+
+
+# fill histograms
+hh.fill(events_hh.run3_dnn_moe_hh, weight =events_hh.event_weight)
+dy.fill(events_dy.run3_dnn_moe_hh, weight =events_dy.event_weight)
+
+# plot histograms
+x = np.linspace(0, 1, n_bins + 1)  # bin edges
+x = (x[:-1] + x[1:]) / 2  # bin centers
+fig = plt.figure(figsize=(10, 6))
+
+# scale the hh histogram up, weighted by the integral of the dy and tt data
+scaling_factor = (hh.values().sum() / (tt.values().sum() + dy.values().sum()))**(-1)
+
+# further split tt bg
+# category id masks (one or two resolved b jets)
+# process id: fh, sl, dl: fully hadronic, semileptonic, di-leptonic
+etau_mask_res1b_sl = ak.any(events_tt.category_ids == 147, axis = 1) & (events_tt.process_id == 1100)
+etau_mask_res1b_dl = ak.any(events_tt.category_ids == 147, axis = 1) & (events_tt.process_id == 1200)
+etau_mask_res1b_fh = ak.any(events_tt.category_ids == 147, axis = 1) & (events_tt.process_id == 1300)
+
+etau_mask_res2b_sl = ak.any(events_tt.category_ids == 151, axis = 1) & (events_tt.process_id == 1100)
+etau_mask_res2b_dl = ak.any(events_tt.category_ids == 151, axis = 1) & (events_tt.process_id == 1200)
+etau_mask_res2b_fh = ak.any(events_tt.category_ids == 151, axis = 1) & (events_tt.process_id == 1300)
+
+mutau_mask_res1b_sl = ak.any(events_tt.category_ids == 175, axis = 1) & (events_tt.process_id == 1100)
+mutau_mask_res1b_dl = ak.any(events_tt.category_ids == 175, axis = 1) & (events_tt.process_id == 1200)
+mutau_mask_res1b_fh = ak.any(events_tt.category_ids == 175, axis = 1) & (events_tt.process_id == 1300)
+
+mutau_mask_res2b_sl = ak.any(events_tt.category_ids == 179, axis = 1) & (events_tt.process_id == 1100)
+mutau_mask_res2b_dl = ak.any(events_tt.category_ids == 179, axis = 1) & (events_tt.process_id == 1200)
+mutau_mask_res2b_fh = ak.any(events_tt.category_ids == 179, axis = 1) & (events_tt.process_id == 1300)
+
+tautau_mask_res1b_sl = ak.any(events_tt.category_ids == 203, axis = 1) & (events_tt.process_id == 1100)
+tautau_mask_res1b_dl = ak.any(events_tt.category_ids == 203, axis = 1) & (events_tt.process_id == 1200)
+tautau_mask_res1b_fh = ak.any(events_tt.category_ids == 203, axis = 1) & (events_tt.process_id == 1300)
+
+tautau_mask_res2b_sl = ak.any(events_tt.category_ids == 207, axis = 1) & (events_tt.process_id == 1100)
+tautau_mask_res2b_dl = ak.any(events_tt.category_ids == 207, axis = 1) & (events_tt.process_id == 1200)
+tautau_mask_res2b_fh = ak.any(events_tt.category_ids == 207, axis = 1) & (events_tt.process_id == 1300)
+
+
+# prepare plotting loop
+masks = [[etau_mask_res1b_sl, etau_mask_res1b_dl, etau_mask_res1b_fh],
+         [etau_mask_res2b_sl, etau_mask_res2b_dl, etau_mask_res2b_fh],
+         [mutau_mask_res1b_sl, mutau_mask_res1b_dl, mutau_mask_res1b_fh],
+         [mutau_mask_res2b_sl, mutau_mask_res2b_dl, mutau_mask_res2b_fh],
+         [tautau_mask_res1b_sl, tautau_mask_res1b_dl, tautau_mask_res1b_fh],
+         [tautau_mask_res2b_sl, tautau_mask_res2b_dl, tautau_mask_res2b_fh]]
+labels = ["etau, res 1b","etau, res 2b", "mutau, res 1b", "mutau, res 2b", "tautau, res 1b", "tautau, res 2b"]
+
+for mask, label in zip(masks, labels):
+    #for scale in ('linear', 'log'):
+        # initialize histograms
+        tt_sl =   Hist(hist.axis.Regular(n_bins, 0, 1, name="tt", label="tt"))
+        tt_dl =   Hist(hist.axis.Regular(n_bins, 0, 1, name="tt", label="tt"))
+        tt_fh =   Hist(hist.axis.Regular(n_bins, 0, 1, name="tt", label="tt"))
+        # fill histograms
+        tt_sl.fill(events_tt.run3_dnn_moe_hh[mask[0]], weight =events_tt.event_weight[mask[0]])
+        tt_dl.fill(events_tt.run3_dnn_moe_hh[mask[1]], weight =events_tt.event_weight[mask[1]])
+        tt_fh.fill(events_tt.run3_dnn_moe_hh[mask[2]], weight =events_tt.event_weight[mask[2]])
+
+        # scale the hh histogram up, weighted by the integral of the dy and tt data
+        scaling_factor = (hh.values().sum() / (tt_sl.values().sum() + tt_dl.values().sum() + tt_fh.values().sum() + dy.values().sum()))**(-1)
+        # plot
+        fig = plt.figure(figsize=(10, 6))
+        bottom = np.zeros_like(x)
+        plt.bar(x, tt_sl.values(), width=1/n_bins, bottom=bottom, alpha=0.5, label='tt semi-leptonic',  edgecolor='black')
+        bottom+=tt_sl.values()
+        plt.bar(x, tt_dl.values(), width=1/n_bins, bottom=bottom, alpha=0.5, label='tt di-leptonic',  edgecolor='black')
+        bottom+=tt_dl.values()
+        plt.bar(x, tt_fh.values(), width=1/n_bins, bottom=bottom, alpha=0.5, label='tt fully hadronic',  edgecolor='black')
+        bottom+=tt_fh.values()
+        plt.bar(x, dy.values(), width=1/n_bins, bottom=bottom, alpha=0.5, label='dy', color='red', edgecolor='black')
+        plt.bar(x, hh.values() * scaling_factor, width=1/n_bins, bottom=None, fill=False, label=f'hh x ({scaling_factor:.2f})', color='green', edgecolor='black')
+        plt.gca().set_yscale('log')  # Set y-axis to log scale
+
+        plt.xlabel("HH output node")
+        plt.ylabel("Number of events")
+        plt.title(f"HH output node for signal and background data, {label} channel")
+        plt.legend()
+
+        plt.savefig(f"images_category_id/hh_output_node_histogram_{label}_logscale.png", dpi=300, bbox_inches='tight')
+        #plt.savefig(f"images_category_id/hh_output_node_histogram_{label}_{scale}scale.png", dpi=300, bbox_inches='tight')
+
+        plt.show()
+        tt.reset()
+

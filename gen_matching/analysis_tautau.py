60f531ca6ae172658bbb7673c32c5c9e19aed394
Katharina Herges <katharina-herges@gmx.de>
2026-06-16 17:59:14 +0200

    added a new input file. Now, there are always two tau candidates in the tautau channel. Continued to implement the sl decay analysis.

--- gen_matching/analysis_tautau.py
+++ gen_matching/analysis_tautau.py
@@ -5,8 +5,7 @@ import hist
 from hist import Hist
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-os.makedirs("analysis_mutau", exist_ok=True)
+
 
 """This script analyses the tautau channel and matches tau to gen W children.
 We separate between fully hadronic, semi-leptonic and di-leptonic channels.
@@ -17,7 +16,7 @@ n_bins = 100
 
 eps = 1e-6 # set eps=0 for normal scale
 lower_border = -14# set to 0 for lin scale
-upper_border = 7# set to 1 for lin scale
+upper_border = 9# set to 1 for lin scale
 def logit(x):
     # set this fct to return x for normal scale
     y = np.log((x + eps) / (1 - x + eps))
@@ -29,8 +28,8 @@ def identity(x):
     return x
 func = logit
 
-events_tt = ak.from_parquet("/data/dust/user/wolfmor/hh2bbtautau/background_characterization/20260504/tt_22pre_v14.parquet")
-# events_tt = ak.from_parquet("/data/dust/user/wolfmor/hh2bbtautau/vincent/tt_22pre_v14.parquet")
+# events_tt = ak.from_parquet("/data/dust/user/wolfmor/hh2bbtautau/background_characterization/20260504/tt_22pre_v14.parquet")
+events_tt = ak.from_parquet("/data/dust/user/wolfmor/hh2bbtautau/background_characterization/prod24/tt_22pre_v14.parquet")
 events_tt = events_tt[events_tt.run3_dnn_moe_hh > 0]
 events_tt = events_tt[events_tt.channel_id == 3] # tautau channel
 events_tt_train = events_tt
@@ -92,7 +91,8 @@ plt.savefig("analysis_tautau/dnn_allevents", dpi=300, bbox_inches='tight')
 plt.show()
 
 ##########################################################################################################
-# tau gen matching
+##########################################################################################################
+# dl decay, tau gen matching
 delr1_tau = deltaR(
     events_dl.tau_eta[:],
     events_dl.tau_phi[:],
@@ -117,7 +117,6 @@ delr4_tau = deltaR(
     events_dl.gen_top_w_children_eta[:, 1, 1],
     events_dl.gen_top_w_children_phi[:, 1, 1],
 )
-
 # tau_delrs = np.zeros((len(events_dl), 2))
 # tau_matches = np.zeros((len(events_dl), 2, 2)) # which children
 # nb_of_taus_dl = np.ones(len(events_dl), dtype=np.int_)
@@ -233,6 +232,7 @@ two_matches2e = Hist(hist.axis.Regular(n_bins, lower_border, upper_border, name=
 one_match1tau = Hist(hist.axis.Regular(n_bins, lower_border, upper_border, name="", label=""))
 one_match1e = Hist(hist.axis.Regular(n_bins, lower_border, upper_border, name="", label=""))
 one_match1mu = Hist(hist.axis.Regular(n_bins, lower_border, upper_border, name="", label=""))
+all_events = Hist(hist.axis.Regular(n_bins, lower_border, upper_border, name="", label=""))
 
 two_matches2tau.fill(func(events_2matched_2tau.run3_dnn_moe_hh), weight =events_2matched_2tau.event_weight)
 two_matches1tau1e.fill(func(events_2matched_1tau_1e.run3_dnn_moe_hh), weight =events_2matched_1tau_1e.event_weight)
@@ -241,6 +241,7 @@ two_matches2e.fill(func(events_2matched_2e.run3_dnn_moe_hh), weight =events_2mat
 one_match1tau.fill(func(events_1matchedtau.run3_dnn_moe_hh), weight =events_1matchedtau.event_weight)
 one_match1e.fill(func(events_1match_e_fake.run3_dnn_moe_hh), weight =events_1match_e_fake.event_weight)
 one_match1mu.fill(func(events_1match_mu_fake.run3_dnn_moe_hh), weight =events_1match_mu_fake.event_weight)
+all_events.fill(func(events_dl.run3_dnn_moe_hh), weight =events_dl.event_weight)
 
 # plot
 x = np.linspace(lower_border, upper_border, n_bins + 1)  # bin edges
@@ -255,7 +256,8 @@ ax1.step(x, two_matches1tau1mu.values(), alpha=0.9, label=r"2 matches: 1 $\tau$,
 ax1.step(x, two_matches2e.values(), alpha=0.9, label=r"2 matches: 2 $e$'s fake $\tau$'s", color='tab:orange')
 ax1.step(x, one_match1tau.values(), alpha=0.9, label=r"One matched $\tau$", color='tab:purple')
 ax1.step(x, one_match1e.values(), alpha=0.9, label=r"One matched $e$ fakes $\tau$", color='tab:brown')
-ax1.step(x, one_match1mu.values(), alpha=0.9, label=r"One matched $\mu$ fakes $\tau$", color='red')
+ax1.step(x, one_match1mu.values(), alpha=0.9, label=r"One matched $\mu$ fakes $\tau$", color='mediumseagreen')
+ax1.step(x, all_events.values(), alpha=0.9, label=r"All events", color='red')
 
 ax1.tick_params(axis='y', labelcolor='black')
 ax1.get_legend_handles_labels()
@@ -264,7 +266,7 @@ ax1.set_yscale("log")
 ax1.set_xscale("linear")
 ax1.set_ylim(bottom=1e-1)
 fig.tight_layout()
-plt.title(fr"HH output node; tt background events, tautau channel (which means only dl decay) split in nb of $\tau$ fakes and number of reco $\tau$ matched to gen W children (matching criterion: $\Delta R < ${delr_cut_tau})", wrap=True)
+plt.title(fr"HH output node; tt background events, di-leptonic decay, tautau channel split in nb of $\tau$ fakes and number of reco $\tau$ matched to gen W children (matching criterion: $\Delta R < ${delr_cut_tau})", wrap=True)
 plt.savefig("analysis_tautau/dnn_tau_matching_allevents", dpi=300, bbox_inches='tight')
 plt.show()
 
@@ -276,3 +278,160 @@ two_matches2e.reset()
 one_match1tau.reset()
 one_match1e.reset()
 one_match1mu.reset()
+
+##########################################################################################################
+##########################################################################################################
+# sl decay: tau matching
+from IPython import embed; embed(header="MESSAGE Line 291 | File: analysis_tautau.py")
+had_mask = abs(events_sl.gen_top_w_children_pdgId) < 7
+had_w_eta = ak.flatten(events_sl.gen_top_w_children_eta[had_mask], axis=-1)
+had_w_phi = ak.flatten(events_sl.gen_top_w_children_phi[had_mask], axis=-1)
+
+delr_qq = deltaR(
+    had_w_eta[:, 0],
+    had_w_phi[:, 0],
+    had_w_eta[:, 1],
+    had_w_phi[:, 1],
+)
+events_fatjet = events_sl[delr_qq < 0.6]
+events_2jets  = events_sl[delr_qq >= 0.6]
+
+W_had_eta = ak.flatten(events_2jets.gen_top_w_children_eta[abs(events_2jets.gen_top_w_children_pdgId) < 7], axis=-1)
+W_had_phi = ak.flatten(events_2jets.gen_top_w_children_phi[abs(events_2jets.gen_top_w_children_pdgId) < 7], axis=-1)
+
+# match the two single quarks to reco taus
+delr1_tau = deltaR(
+    events_2jets.tau_eta[:],
+    events_2jets.tau_phi[:],
+    events_2jets.W_had_eta[:, 0, 0],
+    events_2jets.W_had_phi[:, 0, 0],
+)
+delr2_tau = deltaR(
+    events_2jets.tau_eta[:],
+    events_2jets.tau_phi[:],
+    events_2jets.W_had_eta[:, 0, 1],
+    events_2jets.W_had_phi[:, 0, 1],
+)
+delr3_tau = deltaR(
+    events_2jets.tau_eta[:],
+    events_2jets.tau_phi[:],
+    events_2jets.W_had_eta[:, 1, 0],
+    events_2jets.W_had_phi[:, 1, 0],
+)
+delr4_tau = deltaR(
+    events_2jets.tau_eta[:],
+    events_2jets.tau_phi[:],
+    events_2jets.W_had_eta[:, 1, 1],
+    events_2jets.W_had_phi[:, 1, 1],
+)
+
+min_delr_tau1 = np.minimum(delr1_tau, delr2_tau) # first W
+min_delr_tau2 = np.minimum(delr3_tau, delr4_tau) # second W
+delta_rs = np.stack([min_delr_tau1, min_delr_tau2], axis=1)
+delta_rs = ak.Array(delta_rs)
+
+# find good cut value for tau matching by looking at the delR distribution
+all_delrs = ak.concatenate([ak.flatten(delr1_tau),
+                            ak.flatten(delr2_tau),
+                            ak.flatten(delr3_tau),
+                            ak.flatten(delr4_tau)])
+
+alldelr_max = 4.5
+
+alldelr = Hist(hist.axis.Regular(n_bins, 0, alldelr_max, name="", label="delta R"))
+alldelr.fill(all_delrs)
+
+x = np.linspace(0, alldelr_max, n_bins + 1)  # bin edges
+x = (x[:-1] + x[1:]) / 2  # bin centers
+xticks = (0, 0.25, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5)
+fig = plt.figure(figsize=(10, 6))
+plt.bar(x, alldelr.values(), width=(alldelr_max)/n_bins, bottom=None, fill=True,  color='pink', edgecolor='black')
+plt.xticks(xticks)
+plt.xlabel("delta R = $\sqrt{\Delta \eta² + \Delta \phi²}$")
+plt.ylabel("Number of events")
+plt.title(r"Delta R of all reconstructed taus with gen W children, $\tau\tau$ channel, sl decay")
+
+plt.savefig(f"analysis_tautau/sl_alldelrs_tau_distribution", dpi=300, bbox_inches='tight')
+plt.bar(x, alldelr.values(), width=(alldelr_max)/n_bins, bottom=None, fill=True,  color='pink', edgecolor='black')
+plt.xticks(xticks)
+plt.xlabel("delta R = $\sqrt{\Delta \eta² + \Delta \phi²}$")
+plt.ylabel("Number of events")
+plt.show()
+alldelr.reset()
+from IPython import embed; embed(header="MESSAGE Line 346 | File: analysis_tautau.py")
+
+##################################################################################################################
+delta_w1_close_first_child_mask = (delr1_tau < delr2_tau) # shape [(tau1, w1child1), (tau2, w1child1)]
+delta_w1_close_second_child_mask = ~delta_w1_close_first_child_mask # shape [(tau1, w1child2), (tau2, w1child2)]
+delta_w2_close_first_child_mask = (delr3_tau < delr4_tau)
+delta_w2_close_second_child_mask = ~delta_w2_close_first_child_mask
+
+tau_matches_w1 = ak.concatenate((delta_w1_close_first_child_mask, delta_w1_close_second_child_mask), axis=-1)
+tau_matches_w2 = ak.concatenate((delta_w2_close_first_child_mask, delta_w2_close_second_child_mask), axis=-1)
+tau_matches_w1 = ak.unflatten(tau_matches_w1, 2, axis=1) # shape [[(tau1, w1child1), (tau2, w1child1)],[(tau1, w1child2), (tau2, w1child2)]]
+tau_matches_w2 = ak.unflatten(tau_matches_w2, 2, axis=1) # shape [[(tau1, w2child1), (tau2, w2child1)],[(tau1, w2child2), (tau2, w2child2)]]
+
+# re-build arrays in such a way that shape is [[w1 child1, w1 child2],[w2 child 1, w1 child2]] for each tau
+tau1_w1_c1_mask = delta_w1_close_first_child_mask[:, 0]
+tau1_w1_c2_mask = delta_w1_close_second_child_mask[:, 0]
+tau1_w2_c1_mask = delta_w2_close_first_child_mask[:, 0]
+tau1_w2_c2_mask = delta_w2_close_second_child_mask[:, 0]
+tau1_w1_mask = ak.concatenate((ak.unflatten(tau1_w1_c1_mask, 1, axis=0), ak.unflatten(tau1_w1_c2_mask, 1, axis=0)), axis=-1)
+tau1_w2_mask = ak.concatenate((ak.unflatten(tau1_w2_c1_mask, 1, axis=0), ak.unflatten(tau1_w2_c2_mask, 1, axis=0)), axis=-1)
+tau1_mask = ak.unflatten(ak.concatenate((tau1_w1_mask, tau1_w2_mask), axis=-1), 2, axis=1) # shape [[w1 child1, w1 child2],[w2 child 1, w1 child2]]
+
+tau2_w1_c1_mask = delta_w1_close_first_child_mask[:, 1]
+tau2_w1_c2_mask = delta_w1_close_second_child_mask[:, 1]
+tau2_w2_c1_mask = delta_w2_close_first_child_mask[:, 1]
+tau2_w2_c2_mask = delta_w2_close_second_child_mask[:, 1]
+tau2_w1_mask = ak.concatenate((ak.unflatten(tau2_w1_c1_mask, 1, axis=0), ak.unflatten(tau2_w1_c2_mask, 1, axis=0)), axis=-1)
+tau2_w2_mask = ak.concatenate((ak.unflatten(tau2_w2_c1_mask, 1, axis=0), ak.unflatten(tau2_w2_c2_mask, 1, axis=0)), axis=-1)
+tau2_mask = ak.unflatten(ak.concatenate((tau2_w1_mask, tau2_w2_mask), axis=-1), 2, axis=1) # shape [[w1 child1, w1 child2],[w2 child 1, w1 child2]]
+# do the same for delr
+delr_tau1_w1_c1 = delr1_tau[:, 0]
+delr_tau1_w1_c2 = delr2_tau[:, 0]
+delr_tau1_w2_c1 = delr3_tau[:, 0]
+delr_tau1_w2_c2 = delr4_tau[:, 0]
+delr_tau1_w1 = ak.concatenate((ak.unflatten(delr_tau1_w1_c1, 1, axis=0), ak.unflatten(delr_tau1_w1_c2, 1, axis=0)), axis=-1)
+delr_tau1_w2 = ak.concatenate((ak.unflatten(delr_tau1_w2_c1, 1, axis=0), ak.unflatten(delr_tau1_w2_c2, 1, axis=0)), axis=-1)
+delr_tau1 = ak.unflatten(ak.concatenate((delr_tau1_w1, delr_tau1_w2), axis=-1), 2, axis=1) # shape [[w1 child1, w1 child2],[w2 child 1, w1 child2]]
+
+delr_tau2_w1_c1 = delr1_tau[:, 1]
+delr_tau2_w1_c2 = delr2_tau[:, 1]
+delr_tau2_w2_c1 = delr3_tau[:, 1]
+delr_tau2_w2_c2 = delr4_tau[:, 1]
+delr_tau2_w1 = ak.concatenate((ak.unflatten(delr_tau2_w1_c1, 1, axis=0), ak.unflatten(delr_tau2_w1_c2, 1, axis=0)), axis=-1)
+delr_tau2_w2 = ak.concatenate((ak.unflatten(delr_tau2_w2_c1, 1, axis=0), ak.unflatten(delr_tau2_w2_c2, 1, axis=0)), axis=-1)
+delr_tau2 = ak.unflatten(ak.concatenate((delr_tau2_w1, delr_tau2_w2), axis=-1), 2, axis=1) # shape [[w1 child1, w1 child2],[w2 child 1, w1 child2]]
+mask_match_tau1 = ak.any(ak.any(delr_tau1 < delr_cut_tau, axis=-1), axis=-1)
+mask_match_tau2 = ak.any(ak.any(delr_tau2 < delr_cut_tau, axis=-1), axis=-1)
+
+matched_delr_tau1 = delr_tau1[mask_match_tau1]
+matched_delr_tau2 = delr_tau2[mask_match_tau2]
+#
+mask_tau_event = ak.any(ak.any(abs(events_sl.gen_top_w_children_pdgId) == 15, axis=2), axis=1)
+mask_e_event   = ak.any(ak.any(abs(events_sl.gen_top_w_children_pdgId) == 11, axis=2), axis=1)
+mask_mu_event  = ak.any(ak.any(abs(events_sl.gen_top_w_children_pdgId) == 13, axis=2), axis=1)
+mask_two_taus   = ak.all(ak.any(abs(events_sl.gen_top_w_children_pdgId) == 15, axis=2), axis=1)
+mask_two_es   = ak.all(ak.any(abs(events_sl.gen_top_w_children_pdgId) == 11, axis=2), axis=1)
+
+
+events_2matched_2tau     = events_sl[mask_match_tau1 & mask_match_tau2 & mask_two_taus]
+events_2matched_1tau_1e  = events_sl[mask_match_tau1 & mask_match_tau2 & mask_e_event & mask_tau_event]
+events_2matched_1tau_1mu = events_sl[mask_match_tau1 & mask_match_tau2 & mask_mu_event & mask_tau_event]
+events_2matched_2e       = events_sl[mask_match_tau1 & mask_match_tau2 & mask_two_es]
+
+events_1matchedtau = events_dl[ mask_match_tau1 & ~mask_match_tau2 & ak.any(abs(events_dl.gen_top_w_children_pdgId[:, 0]) == 15, axis=1)|# first tau candidate is match and real tau
+                               ~mask_match_tau1 & mask_match_tau2   &ak.any(abs(events_dl.gen_top_w_children_pdgId[:, 1]) == 15, axis=1)]# 2nd tau candidate is match and real tau
+events_1match_e_fake = events_dl[ mask_match_tau1 & ~mask_match_tau2 & ak.any(abs(events_dl.gen_top_w_children_pdgId[:, 0]) == 11, axis=1)|# first tau candidate is match and is e
+                               ~mask_match_tau1 & mask_match_tau2   &ak.any(abs(events_dl.gen_top_w_children_pdgId[:, 1]) == 11, axis=1)]# 2nd tau candidate is match and is e
+events_1match_mu_fake = events_dl[ mask_match_tau1 & ~mask_match_tau2 & ak.any(abs(events_dl.gen_top_w_children_pdgId[:, 0]) == 13, axis=1)|# first tau candidate is match and is e
+                               ~mask_match_tau1 & mask_match_tau2   &ak.any(abs(events_dl.gen_top_w_children_pdgId[:, 1]) == 13, axis=1)]# 2nd tau candidate is match and is e
+# events_no_matches = events_dl[~mask_match_tau1 & ~mask_match_tau2] # dont exist
+
+delr_1match      = delr_tau1[ mask_match_tau1 & ~mask_match_tau2 |
+                             ~mask_match_tau1 &  mask_match_tau2]
+delr2_1match      = delr_tau2[ mask_match_tau1 & ~mask_match_tau2 |
+                             ~mask_match_tau1 &  mask_match_tau2]
+events_1match   = events_dl[ mask_match_tau1 & ~mask_match_tau2 |
+                             ~mask_match_tau1 &  mask_match_tau2]

13d515547017425320730a02d4f6ebef51d7d484
Katharina Herges <katharina-herges@gmx.de>
2026-07-14 18:15:56 +0200

    extended the module script: Now it contents all functions that are required in multiple scripts

--- dnn_evaluation/modules.py
+++ dnn_evaluation/modules.py
@@ -1,6 +1,57 @@
 import numpy as np
 import torch
 
+def logit(x, eps=1e-6, lower_border=-14, upper_border=12):
+    # set this fct to return x for normal scale
+    y = np.log((x + eps) / (1 - x + eps))
+    return np.clip(y, lower_border, upper_border-eps)
+def inverse_logit(y):
+    x = 1 / (1 + np.exp(-y))
+    return np.clip(x, eps, 1 - eps)
+def identity(x):
+    return x
+
+def asimov_significance(s, *b):
+    """
+    Asimov Significance.
+    Approximation coming from asimov for no background uncertainty: https://arxiv.org/abs/1806.00322 eq. 3.2
+    This approximation is unstable for two cases, and thus certain epsilons are introduced to stabilize  it.:
+    It is unstable for no-background (b=0) regions, which is why *eps_b* is chosen as default value.
+    But also when s > (s+b)(ln(1+s/b)), for which *eps_s* increase stability and lower bound the significance.
+
+    Args:
+        s (Hist): Histogram representing signal in bin.
+        b (Hist): Histogram representing background in bin.
+        eps_b (int, optional):background uncertainty. Defaults to 1 to prevent very high sig values.
+        eps_s (_type_, optional): signal uncertainty. Defaults to 1e-9.
+
+    Returns:
+        numpy.ndarray: Asimov Significance with background uncertainty
+    """
+    eps_s = 1e-9
+    eps_b = 1e-9
+    # from IPython import embed; embed(header="MESSAGE Line 33 | File: modules.py")
+    s_count = s.values()
+
+    # for background, negative weights can exist, which is why they are set to 0 for the significance calculation
+    b_count = []
+    for b_hist in b:
+        _b = b_hist.values()
+        neg_mask = _b < 0
+        _b = np.where(neg_mask, 0, _b)
+        b_count.append(_b)
+    b_count = np.sum(b_count, axis=0)
+
+    # sig² for simple sig function:
+    # sig_per_bin = s_count**2 / (b_count + eps)
+    # asimov sig²:
+    s_count = s_count + eps_s
+    b_count = b_count + eps_b
+    sig_per_bin = 2 * ((s_count + b_count) * np.log(1 + s_count / (b_count )) - s_count) # asimov sig fct
+    sig = np.sqrt(np.abs(sig_per_bin))
+    # print("sig_per_bin", sig_per_bin)
+    # print("leave last bin", sig_per_bin[0:-1])
+    return sig
 
 def def_equbin(
     in_distr: torch.tensor,

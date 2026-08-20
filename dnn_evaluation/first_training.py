refactoring almost done; hopefully last TODO: fix log errors in scaling_factor calculation

dnn_evaluation/first_training.py
@@ -57,7 +57,11 @@ data_dnn_outputs = [
     # process_loader.load_process(path_dnn+"/test_dl2sl2.pt", label="112", description="dl and sl W decay mode oversampled: (1,2,2)"),
     # process_loader.load_process(path_dnn+"/test_dl1sl2.pt", label="121", description="sl W decay mode oversampled: (1,2,1)"),
       # sanity check: old DNNS
-    process_loader.load_process(path_old_dnn+"/tt_22pre_v14.parquet", label="input_file",description="")
+    process_loader.load_process([path_old_dnn+"/tt_22pre_v14.parquet",
+                                path_old_dnn+"/hh_22pre_v14.parquet",
+                                path_old_dnn+"/dy_22pre_v14.parquet"
+                                ],
+                                 label="input_file", description="original input file")
 ]
 print(colored ("data loaded, starting analysis now.", "yellow"))
 for d in data_dnn_outputs:
@@ -67,7 +71,6 @@ for d in data_dnn_outputs:
         # access all events
         # split the tt bg data in three processes
         # events_dict = d.get_events(dataset)
-        from IPython import embed; embed(header="MESSAGE Line 70 | File: first_training.py")
         hists = [HistFab("all_tt_hist", ["tt_dl", "tt_sl", "tt_fh"], "red", "tt: all events", flavor=d.flavor),
                  HistFab("sl_hist", ["tt_sl"], 'green', "tt: sl events", flavor=d.flavor),
                  HistFab("dl_hist", ["tt_dl"], 'blue', "tt: dl events", flavor=d.flavor),
@@ -81,16 +84,13 @@ for d in data_dnn_outputs:
         # lower_border, upper_border, bin_edges, bin_centers = d.get_flats_binedges(dataset, n_bins=10)
         # x = bin_edges
         # x_bin_centers = (x[:-1] + x[1:]) / 2
-
         histograms = {}
         for h in hists:
             histogram = h.create_hist(n_bins, lower_border, upper_border)
-            for key in h.event_keys:
-                h.fill_hist(
-                    histogram,
-                    func,
-                    d
-                    )
+            h.fill_hist(
+                histogram,
+                func,
+                d)
             histograms[h.name] = histogram
 
         sig_all, error_sig_all = asimov_significance(histograms["hh_hist"], histograms["dy_hist"], histograms["fh_hist"], histograms["dl_hist"], histograms["sl_hist"], error_type="poisson_weighted")
@@ -100,7 +100,7 @@ for d in data_dnn_outputs:
         all_significances = [sig_all, sig_dl, sig_sl, sig_fh]
         all_errors = [error_sig_all, error_sig_dl, error_sig_sl, error_sig_fh]
         all_sig_tot = [np.sqrt(np.sum(np.square(s))) for s in all_significances]
-        scaling_factor = ((add_flow_bin(histograms["hh_hist"]).sum())/ (add_flow_bin(histograms["all_tt_hist"]).sum() + add_flow_bin(histograms["dy_hist"]).sum()))**(-1)
+        scaling_factor = ((add_flow_bin(histograms["hh_hist"]).sum()+eps)/ (add_flow_bin(histograms["all_tt_hist"]).sum() + add_flow_bin(histograms["dy_hist"]).sum())+eps)**(-1)
 
         x = np.linspace(lower_border, upper_border, n_bins + 1)  # bin edges
         x = (x[:-1] + x[1:]) / 2  # bin centers

dnn_evaluation/structures.py
@@ -35,14 +35,17 @@ class ProcessLoader:
         flavor = path.split(".")[-1]
         return flavor
 
-    def load_process(self, path, label, description, **kwargs):
-        flavor = self.get_flavor(path)
+    def load_process(self, path, label, description):
+        # if input is only one str, turn it into array
+        if isinstance(path, (str, Path)):
+            path = [path]
+        flavor = self.get_flavor(path[0])
 
         if flavor == "parquet":
             events = self._register_columnflow_array(path, label, description)
             return events
         elif flavor == "pt":
-            events = self._register_pt_array(path, label, description)
+            events = self._register_pt_array(path[0], label, description)
             return events
         else:
             raise ValueError(f"Unsupported file type: {file_type}")
@@ -81,27 +84,27 @@ class ProcessLoader:
         )
 
     def _register_columnflow_array(self, path, label, description):
-        data = ak.from_parquet(path)
-        filter_default = data.run3_dnn_moe_hh > 0
-        data = data[filter_default]
-
-        # default plotting is in logit space; otherwise change func to identity
-        # convert_to_logit = lambda x: func(x.run3_dnn_moe_hh)
-
-        unique_process_id = list(sorted(np.unique(data.process_id)))
         events = {}
-        if 1100 in unique_process_id:
-            label = "tt"
-            events["tt_dl"] = data[data.process_id == 1200]
-            events["tt_fh"] = data[data.process_id == 1300]
-            events["tt_sl"] = data[data.process_id == 1100]
+        for p in path:
+            data = ak.from_parquet(p)
+            filter_default = data.run3_dnn_moe_hh > 0
+            data = data[filter_default]
+            # default plotting is in logit space; otherwise change func to identity
+            # convert_to_logit = lambda x: func(x.run3_dnn_moe_hh)
 
-        elif 21101 in unique_process_id:
-            events["hh"] = data[data.process_id == 21101]
-            label = "hh_kl1kt1"
-        else:
-            label = "dy"
-            events["dy"] = data
+            unique_process_id = list(sorted(np.unique(data.process_id)))
+            if 1100 in unique_process_id:
+                tag = "tt"
+                events["tt_dl"] = data[data.process_id == 1200]
+                events["tt_fh"] = data[data.process_id == 1300]
+                events["tt_sl"] = data[data.process_id == 1100]
+
+            elif 21101 in unique_process_id:
+                events["hh"] = data[data.process_id == 21101]
+                tag = "hh_kl1kt1"
+            else:
+                tag = "dy"
+                events["dy"] = data
         return Process(
             events=events,
             label=label,
@@ -185,15 +188,24 @@ class HistFab:
 
     def fill_hist(self, h, func, events):
         if self.flavor == "torch_tensor":
-            h.fill(
-                func(events.events[key]["scores"].numpy()[:, 0]),
-                weight=events.events[key]["event_weight"].numpy() * events.events[key]["normalization_weights"].numpy()
-            )
-        if flavor == "ak_array":
-            h.fill(
-                func()
-            )
-
+            for key in self.event_keys:
+                h.fill(
+                    func(events.events[key]["scores"].numpy()[:, 0]),
+                    weight=events.events[key]["event_weight"].numpy() * events.events[key]["normalization_weights"].numpy()
+                )
+        if self.flavor == "ak_array":
+            for key in self.event_keys:
+                if func == "logit":
+                    convert_to_logit = lambda x: func(x.run3_dnn_moe_hh)
+                    h.fill(
+                        convert_to_logit(events.events[key]),
+                        weight=events.events[key[0]].event_weight
+                    )
+                if func == "identity":
+                    h.fill(
+                        events.events[key].run3_dnn_moe_hh,
+                        weight=events.events[key[0]].event_weight
+                    )
         return hist
 
     def reset_hist(self, *hists):
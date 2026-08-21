1d2edc8239103c205af5f10c7dbc11632c2f235b
Katharina Herges <katharina-herges@gmx.de>
2026-08-21 18:08:13 +0200

    finished refactoring, added working script to current tag

---
+++ dnn_evaluation/refactored_first_training.py
@@ -0,0 +1,234 @@
+import itertools
+from multiprocessing.util import debug
+import torch
+import awkward as ak
+import hist
+from hist import Hist
+import numpy as np
+import matplotlib.pyplot as plt
+import functools
+import operator
+from termcolor import colored
+from IPython import embed
+from modules import logit, identity, asimov_significance, flats_binning, add_flow_bin
+from structures import ProcessLoader, Process, HistFab
+
+"""This script analyses the first trained DNN's which sample the tt background in different ways concerning their W decay mode,
+meaning di-leptonic, semi-leptonic and full-hadronic W decay, for a flat-s binning. The Asimov significance is computed.
+"""
+n_bins = 10
+eps = 1e-6 # set eps=0 for normal scale
+lower_border_flats = -1e2# set to 0 for lin scale
+# upper_border = 12# set to 1 for lin scale
+func = identity
+
+label_color = "#BE185D"
+"#008C95"
+
+colors = [
+"#F28EBC",  # light pink
+"#D45087",  # rose
+"#9B5DE5",  # lavender violet
+"#5A189A",  # dark violet
+]
+
+# to get better error messages:
+np.seterr(invalid="raise")
+
+path_dnn = "/data/dust/user/hergesk/HH_DNN/evaluation"
+path_old_dnn = "/data/dust/user/wolfmor/hh2bbtautau/background_characterization/prod24"
+process_loader = ProcessLoader()
+
+data_dnn_outputs = [
+    # my DNNs oversampling dl
+    process_loader.load_process(path_dnn+"/alleras_tt_111_a.pt", label="alleras_largelr", description="Weighting: (1,1,1); cosine-shaped LR"),
+      # sanity check: Marcel's old DNN
+    process_loader.load_process([path_old_dnn+"/tt_22pre_v14.parquet",
+                                path_old_dnn+"/hh_22pre_v14.parquet",
+                                path_old_dnn+"/dy_22pre_v14.parquet"
+                                ],
+                                 label="input_file", description="original input file")
+]
+print(colored ("data loaded, starting analysis now.", "yellow"))
+for d in data_dnn_outputs:
+    print(f"Processing label {d.label}")
+    for dataset in ["training", "validation", "test"]:
+        print("processing dataset: ", dataset)
+        # access all events
+        # split the tt bg data in three processes
+        # events_dict = d.get_events(dataset)
+        hists = [HistFab("all_tt_hist", ["tt_dl", "tt_sl", "tt_fh"], "red", "tt: all events", flavor=d.flavor),
+                 HistFab("sl_hist", ["tt_sl"], "#009E73", "tt: sl events", flavor=d.flavor),
+                 HistFab("dl_hist", ["tt_dl"], "tab:orange", "tt: dl events", flavor=d.flavor),# or
+                 HistFab("fh_hist", ["tt_fh"], 'tab:brown', "tt: fh events", flavor=d.flavor),
+                 HistFab("dy_hist", ["dy"], '#3B5B92', "dy: all events", flavor=d.flavor),
+                 HistFab("hh_hist", ["hh"], "black", "hh: all events", flavor=d.flavor)
+        ]
+
+        # get bin edges for flat s binning
+        if d.flavor == "torch_tensor":
+            bin_edges = flats_binning(d.events["hh"]["scores"][:, 0], bin_num = n_bins, hist_edge_l=lower_border_flats)[2]
+        if d.flavor == "ak_array":
+            bin_edges = flats_binning(torch.from_numpy(ak.to_numpy(logit(d.events["hh"].run3_dnn_moe_hh))), bin_num = n_bins, hist_edge_l=lower_border_flats)[2]
+        bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+        lower_border = bin_edges[0]
+        upper_border = bin_edges[-1]
+        histograms = {}
+        for h in hists:
+            histogram = h.create_hist_flats(bin_edges)
+            h.fill_hist(
+                histogram,
+                func,
+                d)
+            histograms[h.name] = histogram
+        sig_all, error_sig_all = asimov_significance(histograms["hh_hist"], histograms["dy_hist"], histograms["fh_hist"], histograms["dl_hist"], histograms["sl_hist"], error_type="poisson_weighted")
+        sig_dl, error_sig_dl = asimov_significance(histograms["hh_hist"], histograms["dl_hist"], error_type="poisson_weighted")
+        sig_sl, error_sig_sl = asimov_significance(histograms["hh_hist"], histograms["sl_hist"], error_type="poisson_weighted")
+        sig_fh, error_sig_fh = asimov_significance(histograms["hh_hist"], histograms["fh_hist"], error_type="poisson_weighted")
+        all_significances = [sig_all, sig_dl, sig_sl, sig_fh]
+        all_errors = [error_sig_all, error_sig_dl, error_sig_sl, error_sig_fh]
+        all_sig_tot = [np.sqrt(np.sum(np.square(s))) for s in all_significances]
+        scaling_factor = ((add_flow_bin(histograms["hh_hist"]).sum()+eps)/ (add_flow_bin(histograms["all_tt_hist"]).sum() + add_flow_bin(histograms["dy_hist"]).sum())+eps)**(-1)
+        x = np.linspace(lower_border, upper_border, n_bins + 1)  # bin edges
+        x = (x[:-1] + x[1:]) / 2  # bin centers
+        # ---
+        x_lin_binedges = np.linspace(lower_border, upper_border, n_bins + 1)  # bin edges
+        x_lin_bincenters = (x_lin_binedges[:-1] + x_lin_binedges[1:]) / 2  # bin centers
+        fig, ax1 = plt.subplots(figsize=(9, 5))
+        fig.subplots_adjust(right=0.85)
+        # TODO: loop through hists instead of this error-prone stuff I'm doung here
+        # ax1.stairs(add_flow_bin(histograms["all_tt_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor='red', label=r"tt: all events")
+        ax1.stairs(add_flow_bin(histograms["sl_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor=hists[1].color, label=r"tt: sl decay")
+        ax1.stairs(add_flow_bin(histograms["dl_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor=hists[2].color, label=r"tt: dl decay")
+        ax1.stairs(add_flow_bin(histograms["fh_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor=hists[3].color, label=r"tt: fh decay")
+        ax1.stairs(add_flow_bin(histograms["dy_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, linestyle=":", fill=False, edgecolor=hists[4].color, label=r"dy")
+        ax1.stairs(add_flow_bin(histograms["hh_hist"])*scaling_factor, edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor=hists[5].color, label=fr"signal x {round(scaling_factor)}")
+
+        ax1.tick_params(axis='y', labelcolor='black')
+
+        yaxis_sig = ax1.twinx()
+        yaxis_sig.set_ylabel(r'asimov significance $Z_A$', color=label_color)
+        # for sig, error, sig_tot, label, color in zip(all_significances,
+        #                                         all_errors,
+        #                                         all_sig_tot,
+        #                                         [r"$Z_A$: tt+dy", r"$Z_A$: tt dl", r"$Z_A$: tt sl", r"$Z_A$: tt fh"],
+        #                                         colors):
+        #     yaxis_sig.errorbar(x_lin_bincenters, sig, yerr=error, label=label+f"; total: {round(sig_tot, 2)}", color=color, alpha=1.0, elinewidth=0.5, capsize=2)# , errorevery=2
+        yaxis_sig.errorbar(x_lin_bincenters, sig_all, yerr=error_sig_all, label=fr"$Z_A$; total: {round(all_sig_tot[0], 2)}", color=label_color, alpha=1.0, elinewidth=0.5, capsize=2)# , errorevery=2
+        yaxis_sig.tick_params(axis='y', labelcolor=label_color)
+        lines1, labels1 = ax1.get_legend_handles_labels()
+        lines2, labels2 = yaxis_sig.get_legend_handles_labels()
+        ax1.set_yscale("log")
+        yaxis_sig.set_yscale("log")
+        ax1.set_ylabel('Number of events', color="black")
+        # lower x axis with bin edges
+        ax1.set_xticks(x_lin_binedges)  # Set label locations.
+        ax1.set_xticklabels(x_lin_binedges.round(2), rotation=45)  # Set text labels.
+        ax1.set_xlabel('HH output node')
+
+        # upper x axis with bin numbers
+        ax1_upper = ax1.twiny()
+        ax1_upper.set_xlim(ax1.get_xlim())
+        ax1_upper.set_xticks(x_lin_bincenters)
+        ax1_upper.set_xticklabels(range(1,len(x_lin_binedges)), rotation=0)  # Set text labels.
+        ax1_upper.set_xlabel('bin number')
+
+        yaxis_sig.legend(lines1 + lines2, labels1 + labels2, loc='upper right', bbox_to_anchor=(1.38, 1))
+        # plt.legend(fontsize="small")
+        # ax1.set_ylim(bottom=1e-1)
+        # fig.tight_layout()
+        plt.title(fr"HH output node for signal ($\kappa_\lambda = 1, \kappa_t = 1$) and background; {d.description}; {dataset} data", wrap=True, pad=13)
+        plt.savefig(f"images_ref/first_training_{d.label}_{dataset}_ttdy", dpi=300, bbox_inches='tight')
+        plt.show()
+        plt.close()
+
+
+################################################################################################
+################################################################################################
+################################################################################################
+# loop without flat-s binning:
+# lower_border = -14
+# upper_border = 11
+
+# for d in data_dnn_outputs:
+#     print(f"Processing label {d.label}")
+#     for dataset in ["training", "validation", "test"]:
+#         print("processing dataset: ", dataset)
+#         # access all events
+#         # split the tt bg data in three processes
+#         # events_dict = d.get_events(dataset)
+#         hists = [HistFab("all_tt_hist", ["tt_dl", "tt_sl", "tt_fh"], "red", "tt: all events", flavor=d.flavor),
+#                  HistFab("sl_hist", ["tt_sl"], 'green', "tt: sl events", flavor=d.flavor),
+#                  HistFab("dl_hist", ["tt_dl"], 'blue', "tt: dl events", flavor=d.flavor),
+#                  HistFab("fh_hist", ["tt_fh"], 'tab:brown', "tt: fh events", flavor=d.flavor),
+#                  HistFab("dy_hist", ["dy"], 'tab:orange', "dy: all events", flavor=d.flavor),
+#                  HistFab("hh_hist", ["hh"], "black", "hh: all events", flavor=d.flavor)
+#         ]
+
+#         histograms = {}
+#         for h in hists:
+#             histogram = h.create_hist(n_bins, lower_border, upper_border)
+#             h.fill_hist(
+#                 histogram,
+#                 func,
+#                 d)
+#             histograms[h.name] = histogram
+#         sig_all, error_sig_all = asimov_significance(histograms["hh_hist"], histograms["dy_hist"], histograms["fh_hist"], histograms["dl_hist"], histograms["sl_hist"], error_type="poisson_weighted")
+#         sig_dl, error_sig_dl = asimov_significance(histograms["hh_hist"], histograms["dl_hist"], error_type="poisson_weighted")
+#         sig_sl, error_sig_sl = asimov_significance(histograms["hh_hist"], histograms["sl_hist"], error_type="poisson_weighted")
+#         sig_fh, error_sig_fh = asimov_significance(histograms["hh_hist"], histograms["fh_hist"], error_type="poisson_weighted")
+#         all_significances = [sig_all, sig_dl, sig_sl, sig_fh]
+#         all_errors = [error_sig_all, error_sig_dl, error_sig_sl, error_sig_fh]
+#         all_sig_tot = [np.sqrt(np.sum(np.square(s))) for s in all_significances]
+#         scaling_factor = ((add_flow_bin(histograms["hh_hist"]).sum()+eps)/ (add_flow_bin(histograms["all_tt_hist"]).sum() + add_flow_bin(histograms["dy_hist"]).sum())+eps)**(-1)
+
+#         x = np.linspace(lower_border, upper_border, n_bins + 1)  # bin edges
+#         x = (x[:-1] + x[1:]) / 2  # bin centers
+#         # ---
+#         x_lin_binedges = np.linspace(lower_border, upper_border, n_bins + 1)  # bin edges
+#         x_lin_bincenters = (x_lin_binedges[:-1] + x_lin_binedges[1:]) / 2  # bin centers
+#         fig, ax1 = plt.subplots(figsize=(9, 5))
+#         fig.subplots_adjust(right=0.85)
+#         ax1.stairs(add_flow_bin(histograms["all_tt_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor='red', label=r"tt: all events")
+#         ax1.stairs(add_flow_bin(histograms["sl_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor='green', label=r"tt: sl decay")
+#         ax1.stairs(add_flow_bin(histograms["dl_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor='blue', label=r"tt: dl decay")
+#         ax1.stairs(add_flow_bin(histograms["fh_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor='tab:brown', label=r"tt: fh decay")
+#         ax1.stairs(add_flow_bin(histograms["dy_hist"]), edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor='tab:orange', label=r"dy")
+#         ax1.stairs(add_flow_bin(histograms["hh_hist"])*scaling_factor, edges = x_lin_binedges, linewidth=1.5, baseline=0, fill=False, edgecolor="black", label=fr"signal x {round(scaling_factor)}")
+
+#         ax1.tick_params(axis='y', labelcolor='black')
+
+#         yaxis_sig = ax1.twinx()
+#         yaxis_sig.set_ylabel(r'asimov significance $Z_A$', color=label_color)
+#         for sig, error, sig_tot, label, color in zip(all_significances,
+#                                                 all_errors,
+#                                                 all_sig_tot,
+#                                                 [r"$Z_A$: tt+dy", r"$Z_A$: tt dl", r"$Z_A$: tt sl", r"$Z_A$: tt fh"],
+#                                                 colors):
+#             yaxis_sig.errorbar(x_lin_bincenters, sig, yerr=error, label=label+f"; total: {round(sig_tot, 2)}", color=color, alpha=1.0, elinewidth=0.5, capsize=2)# , errorevery=2
+#         yaxis_sig.tick_params(axis='y', labelcolor=label_color)
+#         lines1, labels1 = ax1.get_legend_handles_labels()
+#         lines2, labels2 = yaxis_sig.get_legend_handles_labels()
+#         ax1.set_yscale("log")
+#         yaxis_sig.set_yscale("log")
+#         ax1.set_ylabel('Number of events', color="black")
+#         # lower x axis with bin edges
+#         ax1.set_xticks(x_lin_binedges)  # Set label locations.
+#         ax1.set_xticklabels(x_lin_binedges.round(2), rotation=45)  # Set text labels.
+#         ax1.set_xlabel('HH output node')
+
+#         # upper x axis with bin numbers
+#         ax1_upper = ax1.twiny()
+#         ax1_upper.set_xlim(ax1.get_xlim())
+#         ax1_upper.set_xticks(x_lin_bincenters)
+#         ax1_upper.set_xticklabels(range(1,len(x_lin_binedges)), rotation=0)  # Set text labels.
+#         ax1_upper.set_xlabel('bin number')
+
+#         yaxis_sig.legend(lines1 + lines2, labels1 + labels2, loc='upper right', bbox_to_anchor=(1.45, 1))
+#         # plt.legend(fontsize="small")
+#         # ax1.set_ylim(bottom=1e-1)
+#         # fig.tight_layout()
+#         plt.title(fr"HH output node for signal ($\kappa_\lambda = 1, \kappa_t = 1$) and background; {d.description}; {dataset} data; total Asimov significance: $Z_A$ = {round(all_sig_tot[0], 5)}", wrap=True, pad=13)
+#         plt.savefig(f"images_ref/first_training_{d.label}_{dataset}_ttdy_regularbinning", dpi=300, bbox_inches='tight')
+#         plt.show()
+#         plt.close()
---
+++ dnn_evaluation/structures.py
@@ -0,0 +1,209 @@
+import numpy as np
+import torch
+import functools
+import operator
+import hist
+from hist import Hist
+from dataclasses import dataclass
+from modules import flats_binning
+from pathlib import Path
+import awkward as ak
+
+from modules import logit
+
+
+class ProcessLoader:
+    """
+    load signal and background events from two possible input types: pytorch tensor and columflow ak array
+    and bring it in the shape we need:
+    - hh: signal
+    - dy: background
+    - tt: background split in W decay mode
+        -> tt dl
+        -> tt sl
+        -> tt fh
+
+    Input: Data path and label (torch tensor and ak array origin both allowed)
+    Output: sorted data
+    """
+
+    def __init__(self):
+        self.processes = {}
+
+    @staticmethod
+    def get_flavor(path):
+        flavor = path.split(".")[-1]
+        return flavor
+
+    def load_process(self, path, label, description):
+        # if input is only one str, turn it into array
+        if isinstance(path, (str, Path)):
+            path = [path]
+        flavor = self.get_flavor(path[0])
+        if flavor == "parquet":
+            events = self._register_columnflow_array(path, label, description)
+            return events
+        elif flavor == "pt":
+            events = self._register_pt_array(path[0], label, description)
+            return events
+        else:
+            raise ValueError(f"Unsupported file type: {file_type}")
+
+    def _register_pt_array(self, path, label, description):
+        data = torch.load(path, map_location=torch.device("cpu"))
+        dy_indices = []  # collect all dy indices
+        # dy_dict = {} # new dict to store all dy data together
+        for dataset in ["training", "validation", "test"]:
+            dy_indices = [k[1] for k in data[0][dataset].keys() if k[0] == "dy"]
+            dicts = [(data[0][dataset][("dy", i)]) for i in dy_indices]
+
+            # TODO: code is error-prone as new columns added to the NN output will not be adopted immediately
+            events_dy = {
+                "scores": torch.cat([d["scores"] for d in dicts]),
+                "event_weight": torch.cat([d["event_weight"] for d in dicts]),
+                "normalization_weights": torch.cat(
+                    [d["normalization_weights"] for d in dicts]
+                ),
+                "event_id": torch.cat([d["event_id"] for d in dicts]),
+            }
+
+        # store all categories in a dict:
+        data = {
+            "tt_dl": data[0][dataset][("tt", 1200)],
+            "tt_fh": data[0][dataset][("tt", 1300)],
+            "tt_sl": data[0][dataset][("tt", 1100)],
+            "hh": data[0][dataset][("hh", 21101)],
+            "dy": events_dy,
+        }
+        return Process(
+            events=data,
+            label=label,
+            description=description,
+            flavor="torch_tensor"
+        )
+
+    def _register_columnflow_array(self, path, label, description):
+        events = {}
+        for p in path:
+            data = ak.from_parquet(p)
+            filter_default = data.run3_dnn_moe_hh > 0
+            data = data[filter_default]
+            # default plotting is in logit space; otherwise change func to identity
+            # convert_to_logit = lambda x: func(x.run3_dnn_moe_hh)
+
+            unique_process_id = list(sorted(np.unique(data.process_id)))
+            if 1100 in unique_process_id:
+                tag = "tt"
+                events["tt_dl"] = data[data.process_id == 1200]
+                events["tt_fh"] = data[data.process_id == 1300]
+                events["tt_sl"] = data[data.process_id == 1100]
+
+            elif 21101 in unique_process_id:
+                events["hh"] = data[data.process_id == 21101]
+                tag = "hh"
+            else:
+                tag = "dy"
+                events["dy"] = data
+        return Process(
+            events=events,
+            label=label,
+            description=description,
+            flavor="ak_array"
+        )
+
+    def get_flats_binedges(self, dataset, n_bins=10):
+        lower_border_flats = -1e2
+        data = self.events[0][dataset]
+        bin_edges = flats_binning(
+            data[("hh", 21101)]["scores"][:, 0],
+            bin_num=n_bins,
+            hist_edge_l=lower_border_flats,
+        )[2]
+        bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+        # check if two bin edges are the same
+        for i in range(len(bin_edges)):
+            for j in range(len(bin_edges)):
+                if (i != j) & (bin_edges[i] == bin_edges[j]):
+                    print(
+                        "\033[93mError: Two bin edges are the same! Check bin edges and delete one of the doubles!\033[0m"
+                    )
+        # important: map hist edges to bin edges from flat-s binning
+        lower_border = bin_edges[0]
+        upper_border = bin_edges[-1]
+        return lower_border, upper_border, bin_edges, bin_centers
+
+
+@dataclass
+class Process:
+    """
+    Class to store the datasets to process, together with its labels."""
+
+    events: object
+    label: str
+    description: str
+    flavor: str
+
+
+@dataclass
+class HistFab:
+    """
+    Class to store histogram configurations and produce hists."""
+
+    name: str
+    event_keys: list[str]
+    color: str
+    label: str
+    flavor: str
+
+    def get_hist_config(self):
+        return {
+            "name": self.name,
+            "color": self.color,
+            "label": self.label,
+            "type": self.type,
+        }
+
+    def create_hist(self, n_bins, lower_border, upper_border):
+        """version without flat-s binning."""
+        return Hist(
+            hist.axis.Regular(
+                n_bins,
+                lower_border,
+                upper_border,
+                name=self.name,
+                label=self.label,
+                underflow=True,
+                overflow=True,
+            ),
+            storage=hist.storage.Weight(),
+        )
+
+    def create_hist_flats(self, bin_edges):
+        """version with flat-s binning."""
+        return Hist(
+            hist.axis.Variable(bin_edges, name=self.name, label=self.label, flow=True),
+            storage=hist.storage.Weight(),
+        )
+
+    def fill_hist(self, h, func, events):
+        from modules import logit, identity
+        if self.flavor == "torch_tensor":
+            for key in self.event_keys:
+                h.fill(
+                    func(events.events[key]["scores"].numpy()[:, 0]),
+                    weight=events.events[key]["event_weight"].numpy() * events.events[key]["normalization_weights"].numpy()
+                )
+            if func == logit:
+                print(colored("Warning: logit plots first need to be implemented in HistFab class", red))
+        if self.flavor == "ak_array":
+            for key in self.event_keys:
+                h.fill(
+                    logit(events.events[key].run3_dnn_moe_hh),
+                    weight=events.events[key].event_weight
+                )
+
+        return hist
+
+    def reset_hist(self, *hists):
+        for h in hists:
+            h.reset()

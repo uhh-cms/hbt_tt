d7fb3a7e84569fa52eae4ccfa9f76811d2458bb6
Katharina Herges <katharina-herges@gmx.de>
2026-09-02 21:11:05 +0200

    improved all-sig-in-one-plot plots

--- dnn_evaluation/significance_plots/sig_comparison.py
+++ dnn_evaluation/significance_plots/sig_comparison.py
@@ -20,10 +20,11 @@ from significance_utils import SigLoader
 """This script plots the difference in significance per bin from the baseline,
 one plot for all significances, as a comparison."""
 
-n_bins = 10
+n_bins = 35
 eps = 1e-6 # set eps=0 for normal scale
-lower_border_flats = -1e2# set to 0 for lin scale
-# upper_border = 12# set to 1 for lin scale
+# plotting in logit space:
+lower_border = 0
+upper_border = 11
 func = identity
 
 path_dnn = "/data/dust/user/hergesk/HH_DNN/evaluation"
@@ -42,6 +43,7 @@ data_dnn_outputs = [
 ]
 
 all_sigs_per_bin = {}
+all_sigs_tt_per_bin = {}
 
 print(colored ("data loaded, starting analysis now.", "yellow"))
 for output in data_dnn_outputs:
@@ -53,46 +55,64 @@ for output in data_dnn_outputs:
             HistFab("dy_hist", ["dy"], "tab:orange", "dy: all events", flavor=output.flavor),# '#3B5B92'
             HistFab("hh_hist", ["hh"], "black", "hh: all events", flavor=output.flavor)
     ]
-    # get bin edges for flat s binning
-    if output.flavor == "torch_tensor":
-        bin_edges = flats_binning(output.events["hh"]["scores"][:, 0], bin_num = n_bins, hist_edge_l=lower_border_flats)[2]
-    if output.flavor == "ak_array":
-        bin_edges = flats_binning(torch.from_numpy(ak.to_numpy(logit(output.events["hh"].run3_dnn_moe_hh))), bin_num = n_bins, hist_edge_l=lower_border_flats)[2]
-    bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
-    lower_border = bin_edges[0]
-    upper_border = bin_edges[-1]
     histograms = {}
     for h in hists:
-        histogram = h.create_hist_flats(bin_edges)
+        histogram = h.create_hist(n_bins, lower_border, upper_border)
         h.fill_hist(
             histogram,
             func,
-            output)
+            output.add_btagcut())
         histograms[h.name] = histogram
     tot_sig_all_binned, tot_error_sig_all_binned = asimov_significance(histograms["hh_hist"], histograms["dy_hist"], histograms["fh_hist"], histograms["dl_hist"], histograms["sl_hist"], error_type="poisson_weighted")
+    tot_sig_tt_binned, tot_error_sig_tt_binned = asimov_significance(histograms["hh_hist"], histograms["fh_hist"], histograms["dl_hist"], histograms["sl_hist"], error_type="poisson_weighted")
+
     tot_sig_all = np.sqrt(np.sum(np.square(tot_sig_all_binned)))
+    tot_sig_tt = np.sqrt(np.sum(np.square(tot_sig_tt_binned)))
     all_sigs_per_bin[output.description] = {"per_bin":      tot_sig_all_binned,
                                             "err_per_bin":  tot_error_sig_all_binned,
                                             "total": tot_sig_all}
+    all_sigs_tt_per_bin[output.description] = {"per_bin":      tot_sig_tt_binned,
+                                        "err_per_bin":  tot_error_sig_tt_binned,
+                                        "total": tot_sig_tt}
     
 x_lin_binedges = np.linspace(lower_border, upper_border, n_bins + 1)  # bin edges
 x_lin_bincenters = (x_lin_binedges[:-1] + x_lin_binedges[1:]) / 2  # bin centers
 fig, ax = plt.subplots(figsize=(9, 5))
 fig.subplots_adjust(right=0.85)
 
+### -----
+# plot all (tt+dy) sigs in one plot
 for key in all_sigs_per_bin.keys():
     ax.errorbar(x_lin_bincenters, all_sigs_per_bin[key]["per_bin"] - all_sigs_per_bin["baseline"]["per_bin"], 
                 #yerr=all_sigs_per_bin[key]["err_per_bin"], 
-                label=key+fr"; total $Z_A$: {round(all_sigs_per_bin[key]['total'], 2)}", 
+                label=key+fr"; total: {round(all_sigs_per_bin[key]['total'], 2)}", 
                 alpha=1.0, elinewidth=0.5, capsize=2)# , errorevery=2
 
 ax.set_xlabel("DNN output node score")
-ax.set_ylabel(r"$Z_A$")
-# ax.set_yscale("log")
-ax.set_xticks(x_lin_binedges)
-ax.set_xticklabels(x_lin_binedges.astype(int), rotation=45)
+ax.set_ylabel(r"$\Delta Z_A$ = $Z_{A, DNN} - Z_{A, baseline}$")
+# ax.set_xscale("log")
 
 plt.legend()
-plt.title("Difference in Asimov significance for all tested DNN's")
-plt.savefig(f"images_all_sigs/all_sigs", dpi=300, bbox_inches='tight')
+plt.title("Relative difference in Asimov significance (tt + dy)for all tested DNN's")
+plt.savefig(f"images_all_sigs/all_delta_sig_ttdy", dpi=300, bbox_inches='tight')
+plt.show()
+plt.clf()
+
+### -----
+# plot all tt sigs in one plot
+fig, ax = plt.subplots(figsize=(9, 5))
+fig.subplots_adjust(right=0.85)
+for key in all_sigs_tt_per_bin.keys():
+    ax.errorbar(x_lin_bincenters, all_sigs_tt_per_bin[key]["per_bin"] - all_sigs_tt_per_bin["baseline"]["per_bin"], 
+                #yerr=all_sigs_tt_per_bin[key]["err_per_bin"], 
+                label=key+fr"; total: {round(all_sigs_tt_per_bin[key]['total'], 2)}", 
+                alpha=1.0, elinewidth=0.5, capsize=2)# , errorevery=2
+
+ax.set_xlabel("DNN output node score")
+ax.set_ylabel(r"$\Delta Z_A$ = $Z_{A, DNN} - Z_{A, baseline}$")
+# ax.set_xscale("log")
+
+plt.legend()
+plt.title("Relative difference in Asimov significance (only tt) for all tested DNN's")
+plt.savefig(f"images_all_sigs/all_delta_sig_tt", dpi=300, bbox_inches='tight')
 plt.show()
